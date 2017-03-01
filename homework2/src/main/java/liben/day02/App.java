07836b9c7deaba027d989c2ff0b13e30d1dd1b1c
liben <1123863702@qq.com>
2017-03-01 18:20:05 +0800

    排序

---
+++ homework2/src/main/java/liben/day02/App.java
@@ -0,0 +1,31 @@
+package liben.day02;
+
+public class App {
+
+	private static void QuickSort(int[] array, int start, int end) {
+		if (start < end) {
+			int key = array[start];// 初始化保存基准
+			int i = start, j;// 初始化i,j
+			for (j = start + 1; j <= end; j++) {
+				if (array[j] < key) { // 如果此处元素小于基准，则把此元素和i+1处元素交换，并将i加1，如大于或等于基元则继续循环
+					int temp = array[j];
+					array[j] = array[i + 1];
+					array[i + 1] = temp;
+					i++;
+				}
+			}
+			array[start] = array[i];// 交换i处元素和基准
+			array[i] = key;
+			QuickSort(array, start, i - 1);// 递归调用
+			QuickSort(array, i + 1, end);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] array = new int[] { 11, 213, 134, 44, 77, 78, 23, 43 };
+		QuickSort(array, 0, array.length - 1);
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("第"+(i + 1) + "名:" + array[i]);
+		}
+	}
+}
